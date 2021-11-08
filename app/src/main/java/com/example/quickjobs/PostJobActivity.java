ec4b39b45d05a15499f37dd6c488c0cff1bb25de
Om Chevli <omchevli@gmail.com>
2021-11-08 15:11:42 +0530

    Recycler View

--- app/src/main/java/com/example/quickjobs/PostJobActivity.java
+++ app/src/main/java/com/example/quickjobs/PostJobActivity.java
@@ -1,6 +1,8 @@
 package com.example.quickjobs;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,16 +10,38 @@ import android.view.View;
 import android.widget.Toolbar;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class PostJobActivity extends AppCompatActivity {
 
     private FloatingActionButton addBtn;
+    private RecyclerView jobIdRecycler;
+
+    //Firebase
+    private FirebaseAuth mAuth;
+    private DatabaseReference jobPostDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_job);
         addBtn=findViewById(R.id.fab_add);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser mUser = mAuth.getCurrentUser();
+        String uId = mUser.getUid();
+
+        jobPostDb = FirebaseDatabase.getInstance().getReference().child("Job Post").child(uId);
+
+        jobIdRecycler=findViewById(R.id.job_post_id_recycler);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setStackFromEnd(true);
+        layoutManager.setReverseLayout(true);
+        jobIdRecycler.setHasFixedSize(true);
+        jobIdRecycler.setLayoutManager(layoutManager);
 
         addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
